7461ff16bdd7362f783f63dc75c501198bf726fd
Lea Devoudelle-Royer <ldevoude@z2r11p1.42lyon.fr>
2025-03-10 08:10:03 +0100

    {feat} Everything is secured, no leaks and structure is working perfectly atm

--- utils.c
+++ utils.c
@@ -14,9 +14,12 @@ void    init_stacks(size_t size_stack_a, t_stacks *s, int *stack_a)
     s->move_cost = ft_calloc(size_stack_a, sizeof(int));
     s->rr = ft_calloc(size_stack_a, sizeof(int));
     s->rrr = ft_calloc(size_stack_a, sizeof(int));
-    // if (!s->array_a ||!s->array_b||!s->target_a||!s->target_b||!s->move_cost
-    //     ||!s->rr||!s->rrr)
-        //TODO need to free everything with a function
+     if (!s->array_a ||!s->array_b||!s->target_a||!s->target_b||!s->move_cost
+        ||!s->rr||!s->rrr)
+    {
+        free(stack_a);
+        free_struct(s);
+    }
     while (i < size_stack_a)
     {
         s->array_a[i] = stack_a[i];
@@ -27,12 +30,19 @@ void    init_stacks(size_t size_stack_a, t_stacks *s, int *stack_a)
 
 void free_struct(t_stacks *s)
 {
-    free(s->array_a);
-    free(s->array_b);
-    free(s->target_a);
-    free(s->target_b);
-    free(s->move_cost);
-    free(s->rr);
-    free(s->rrr);
+    if (s->array_a)
+        free(s->array_a);
+    if (s->array_b)
+        free(s->array_b);
+    if (s->target_a)
+        free(s->target_a);
+    if (s->target_b)
+        free(s->target_b);
+    if (s->move_cost)
+        free(s->move_cost);
+    if (s->rr)
+        free(s->rr);
+    if (s->rrr)
+        free(s->rrr);
     error_message(13, s);
 }
