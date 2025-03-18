ab40f66bee05b444c126d671dc55e90848d5a066
Lea Devoudelle-Royer <ldevoude@z2r11p1.42lyon.fr>
2025-03-18 12:58:03 +0100

    {misc} last push before merge

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 12:54:11 by ldevoude          #+#    #+#             */
+/*   Updated: 2025/03/18 12:55:20 by ldevoude         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "header/push_swap.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    t_stacks     *stacks;
+	t_stacks	*stacks;
 
-    //printf("argc = %d\n", argc);
-    stacks = ft_calloc(1, sizeof(t_stacks));
-    if (!stacks)
-        return (error_message(MALLOC_ERROR, stacks));
-    if(argc < 2)
-        return (error_message(ARGUMENT_ERROR, stacks));
-    error_checker (argv, argc, stacks); 
-    if (!steps_push_swap(stacks))
-        return(error_message(5, stacks));
-    return (0);
+	stacks = ft_calloc(1, sizeof(t_stacks));
+	if (!stacks)
+		return (error_message(MALLOC_ERROR, stacks));
+	if (argc < 2)
+		return (error_message(ARGUMENT_ERROR, stacks));
+	error_checker(argv, argc, stacks);
+	if (!steps_push_swap(stacks))
+		return (error_message(5, stacks));
+	return (0);
 }
-//I do turkish
-//verifier si deja trié ?
