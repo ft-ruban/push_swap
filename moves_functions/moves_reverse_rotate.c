1e71cede7d87cc0bc9ad583b4d8a0ab102e38994
Lea Devoudelle-Royer <ldevoude@z2r10p4.42lyon.fr>
2025-03-20 13:40:27 +0100

    {fix} fixed some functions and made reverse_rotate_ab lighter}

--- moves_functions/moves_reverse_rotate.c
+++ moves_functions/moves_reverse_rotate.c
@@ -15,20 +15,39 @@ void reverse_rotate_a(t_stacks *s, int index_max)
     int buff;
     int i;
 
-    buff = s->array_a[index_max - 1];  // Stocke le dernier élément
+    buff = s->array_a[index_max - 1];
     i = index_max - 1;
-    while (i != 0)  // Décalage des éléments vers la droite
+    while (i != 0)
     {
         s->array_a[i] = s->array_a[i - 1];
         i--;
     }
-    s->array_a[0] = buff;  // Met le dernier élément en première position
+    s->array_a[0] = buff;
     ft_printf("rra\n");
 }
 
-// void reverse_rotate_a(t_stacks *s, int index_max)
+
+
+void reverse_rotate_b(t_stacks *s, int index_max)
+{
+    int buff;
+    int i;
+
+    buff = s->array_b[index_max - 1];
+    i = index_max - 1;
+    while (i != 0)
+    {
+        s->array_b[i] = s->array_b[i - 1];
+        i--;
+    }
+    s->array_b[0] = buff;
+    ft_printf("rrb\n");
+}
+
+// void reverse_rotate_ab(t_stacks *s, int index_max)
 // {
 //     int     buff[index_max];
+//     int     buff_b[s->size_b];
 //     int     i;
 //     int     j;
 
@@ -50,76 +69,46 @@ void reverse_rotate_a(t_stacks *s, int index_max)
 //         j++;
 //     }
 //     s->array_a[0] = buff[i];
-//     ft_printf("rra\n");
-
-void reverse_rotate_b(t_stacks *s, int index_max)
-{
-    int     buff[index_max];
-    int     i;
-    int     j;
-
-    i = 0;
-    j = 0;
-    while ((size_t)j </*=*/ s->size_b)
-    {
-        buff[j] = s->array_b[i];
-        i++;
-        j++;
-    }
-    buff[j] = s->array_b[s->size_b - 1];
-    i = 0;
-    j = 1;
-    while ((size_t)j <= s->size_b)
-    {
-        s->array_b[j] = buff[i];
-        i++;
-        j++;
-    }
-    s->array_b[0] = buff[i];
-    ft_printf("rrb\n");
-}
+//     i = 0;
+//     j = 0;
+//     while ((size_t)j </*=*/ s->size_b)
+//     {
+//         buff_b[j] = s->array_b[i];
+//         i++;
+//         j++;
+//     }
+//     buff_b[j] = s->array_b[s->size_b - 1];
+//     i = 0;
+//     j = 1;
+//     while ((size_t)j <= s->size_b)
+//     {
+//         s->array_b[j] = buff_b[i];
+//         i++;
+//         j++;
+//     }
+//     s->array_b[0] = buff_b[i];
+//     ft_printf("rrr\n");
+// } 
 void reverse_rotate_ab(t_stacks *s, int index_max)
 {
-    int     buff[index_max];
-    int     buff_b[s->size_b];
-    int     i;
-    int     j;
+    int buff;
+    int i;
 
-    i = 0;
-    j = 0;
-    while (j </*=*/ index_max)
+    buff = s->array_a[index_max - 1];
+    i = index_max - 1;
+    while (i != 0)
     {
-        buff[j] = s->array_a[i];
-        i++;
-        j++;
+        s->array_a[i] = s->array_a[i - 1];
+        i--;
     }
-    buff[j] = s->array_a[index_max - 1];
-    i = 0;
-    j = 1;
-    while (j <= index_max)
+    s->array_a[0] = buff;
+    buff = s->array_b[s->size_b - 1];
+    i = s->size_b - 1;
+    while (i != 0)
     {
-        s->array_a[j] = buff[i];
-        i++;
-        j++;
+        s->array_b[i] = s->array_b[i - 1];
+        i--;
     }
-    s->array_a[0] = buff[i];
-    i = 0;
-    j = 0;
-    while ((size_t)j </*=*/ s->size_b)
-    {
-        buff_b[j] = s->array_b[i];
-        i++;
-        j++;
-    }
-    buff_b[j] = s->array_b[s->size_b - 1];
-    i = 0;
-    j = 1;
-    while ((size_t)j <= s->size_b)
-    {
-        s->array_b[j] = buff_b[i];
-        i++;
-        j++;
-    }
-    s->array_b[0] = buff_b[i];
+    s->array_b[0] = buff;
     ft_printf("rrr\n");
 }
